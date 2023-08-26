c99f88c5e68a4c9cc58ed1e948d8d43640228613
soappeea <itsophialin@gmail.com>
2023-08-26 02:05:10 -0400

    AI Implementation

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import openai
+# import pandas as pd
+# import numpy as np
+from getpass import getpass
+from dotenv import load_dotenv #Import dotenv
+import os
+
+
+app = Flask(__name__)
+
+load_dotenv()
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+#Make sure you install python, create a project environment, install flask
+#Use python -m flask run to run, then click on link
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/generate_response", methods=["POST"])
+def generate_response():
+    user_input = request.form["user_input"]
+
+    #Use OpenAI to generate a response
+    response = generate_ai_response(user_input)
+
+    return render_template("index.html", user_input=user_input, response=response)
+
+def generate_ai_response(user_input):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=user_input,
+        max_tokens=100,
+    )
+    
+    return response.choices[0].text
+
+if __name__ == "__main__":
+    app.run(debug=True)
